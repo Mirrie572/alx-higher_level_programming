59c0df30a7fbc2609c8c8f8304b4d16ab903a453
mirrie572 <maggie05072000@gmail.com>
2023-11-01 15:04:42 +0900

    task 11

---
+++ 0x00-python-hello_world/100-write.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+import sys
+# printing to stderr using write
+sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+# the exit to return 1
+sys.exit(1)
