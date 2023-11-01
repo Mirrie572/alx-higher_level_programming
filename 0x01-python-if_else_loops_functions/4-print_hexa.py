c3b6b24b23fd3d37e1952c39ad2dc88a6daaf542
mirrie572 <maggie05072000@gmail.com>
2023-11-01 15:39:40 +0900

    task 4

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-for num in range(99):
-    print(f"{num:d} = {hex(num)}")
+for num in range(0, 99):
+    if num !=122 and num != 101:
+        print("{} = 0x{:x}".format(num, num))
+
