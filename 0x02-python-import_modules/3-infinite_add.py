9a7d2a4dfc55c8b732fc62d1ec2177553e3f7a66
Mirrie572 <maggie05072000@gmail.com>
2023-11-02 07:47:13 -0700

    task 3 redone

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    r = 0 # r ffor results
+    r = 0
     if (len(sys.argv) > 1):
         for i in range(1, len(sys.argv)):
             r += (int(sys.argv[i]))
