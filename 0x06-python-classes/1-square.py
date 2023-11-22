73527fadecfef332c1939f64cc9110069da0e7f1
Mirrie572 <maggie05072000@gmail.com>
2023-11-23 08:48:01 +0900

    source code

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Empty class Square that defines a square."""
+
+
+class Square:
+    """Class Square that defines a square with size validation."""
+    def __init__(self, size=0):
+        self.__size = size
