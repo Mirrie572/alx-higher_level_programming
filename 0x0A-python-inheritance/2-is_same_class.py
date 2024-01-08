9028131039723ce50e8981ffd431bfa6d9f07606
Mirrie572 <maggie05072000@gmail.com>
2024-01-09 01:02:06 +0900

    task 0 to 2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module with a lookup class"""
+
+
+def is_same_class(obj, a_class):
+    """
+        Inherits from list and provides a
+        method to print sorted list.
+    """
+    return type(obj) is a_class
