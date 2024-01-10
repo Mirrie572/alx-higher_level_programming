6211643ab6c0ef015cedb57688635fb86859bf53
Mirrie572 <maggie05072000@gmail.com>
2024-01-10 11:14:51 +0900

    file 13

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -2,19 +2,13 @@
 # This script contains the append_after function.
 
 def append_after(filename="", search_string="", new_string=""):
-    """
-    Appends new_string to a file following each line containing search_string.
-
-    :param filename: Path to the file
-    :param search_string: String to search for within the file
-    :param new_string: String to append after each occurrence of search_string
-    """
+    '''inserts new_string to a file, after find containing search_string'''
     with open(filename, "r+") as file:
         lines = file.readlines()
         changed = []
-        for line in lines:
-            changed.append(line)
-            if search_string in line:
+        for line in range(len(lines)):
+            changed.append(lines[line])
+            if search_string in lines[line]:
                 changed.append(new_string)
 
         file.seek(0)
